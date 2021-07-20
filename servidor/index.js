ce6251d0bf3fba32a5e514e0cee7b3afff702618
Mario González <mario@mariogl.com>
2021-07-20 09:49:43 +0200

    Guardar archivo en disco duro

--- servidor/index.js
+++ servidor/index.js
@@ -1,10 +1,29 @@
 require("dotenv").config();
 const debug = require("debug")("imagenes:servidor");
 const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "imagenes",
+  filename: (req, file, cb) => {
+    const nombreArchivo = `${path.basename(
+      file.originalname,
+      path.extname(file.originalname)
+    )}-${Date.now()}${path.extname(file.originalname)}`;
+    cb(null, `${nombreArchivo}`);
+  },
+});
+const upload = multer({ storage });
 const { error404, errorGeneral } = require("./errores");
 const app = require("./init");
 
 app.use(morgan("dev"));
+
+app.post("/usuarios/registro", upload.single("imagen"), (req, res, next) => {
+  console.log(req.file);
+  res.send("Usuario registrado");
+});
 
 app.use(error404);
 app.use(errorGeneral);
