1ac7b8785b4249fac4d40a09f713f7bd86d7fec6
Mario González <mario@mariogl.com>
2021-07-20 10:35:22 +0200

    Guardar archivo en Firebase

--- servidor/index.js
+++ servidor/index.js
@@ -1,29 +1,56 @@
 require("dotenv").config();
+const admin = require("firebase-admin");
+const cors = require("cors");
 const debug = require("debug")("imagenes:servidor");
 const morgan = require("morgan");
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: "imagenes",
-  filename: (req, file, cb) => {
-    const nombreArchivo = `${path.basename(
-      file.originalname,
-      path.extname(file.originalname)
-    )}-${Date.now()}${path.extname(file.originalname)}`;
-    cb(null, `${nombreArchivo}`);
-  },
+const serviceAccount = require("../proyecto-final-imagenes-firebase-adminsdk-f93fm-e7dd565481.json");
+// JSON descargado desde Firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "proyecto-final-imagenes.appspot.com", // Nombre de vuestro bucket
 });
-const upload = multer({ storage });
+const bucket = admin.storage().bucket();
+const upload = multer();
 const { error404, errorGeneral } = require("./errores");
 const app = require("./init");
 
 app.use(morgan("dev"));
-
-app.post("/usuarios/registro", upload.single("imagen"), (req, res, next) => {
-  console.log(req.file);
-  res.send("Usuario registrado");
-});
+app.use(cors());
+app.post(
+  "/usuarios/registro",
+  upload.single("imagen"),
+  async (req, res, next) => {
+    const nombreArchivo = `${path.basename(
+      req.file.originalname,
+      path.extname(req.file.originalname)
+    )}_${Date.now()}${path.extname(req.file.originalname)}`;
+    const datos = bucket.file(nombreArchivo);
+    const existe = await datos.exists();
+    if (existe[0]) {
+      // Capturamos y manejamos el error
+      console.log("El archivo ya existe");
+      return;
+    }
+    const ficheroFB = datos.createWriteStream({ resumable: false });
+    ficheroFB.end(req.file.buffer);
+    ficheroFB.on("error", (err) => {
+      // Capturamos y manejamos el error
+      console.log(err.message);
+    });
+    ficheroFB.on("finish", () => {
+      // El archivo se ha subido correctamente
+      // La url:      console.log(`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${datos.name}?alt=media`);
+      console.log("Archivo guardado en FireBase");
+      console.log(
+        `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${datos.name}?alt=media`
+      );
+    });
+    res.send("Usuario registrado");
+  }
+);
 
 app.use(error404);
 app.use(errorGeneral);
